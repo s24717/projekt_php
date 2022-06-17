c927c05514cb75e90ffadad1ce531223250910d8
s24717 <s24717@pjwstk.edu.pl>
2022-06-17 22:07:18 +0200

    3
    warunek polaczenia sie z baza danych

--- zaloguj.php
+++ zaloguj.php
@@ -1,8 +1,21 @@
 <?php
 
-    $login = $_POST['login'];
-    $haslo = $_POST['haslo'];
+    require_once "connect.php"; //wlaczamy plik connect.php do zaloguj.php (once pyta czy juz czasem wczesniej nie zostal dodany)
 
+    $polaczenie = @$new mysqli($host,$db_user,$db_password,$db_name);
+
+    if($polaczenie->connect_errno!=0)
+    {
+        echo "Error: ".$polaczenie->connect_errno." Opis: ".$polaczenie->connect_error;
+    }
+    else
+    {
+        $login = $_POST['login'];
+        $haslo = $_POST['haslo'];
+
+
+        $polaczenie->close();
+    }
 
     
 
