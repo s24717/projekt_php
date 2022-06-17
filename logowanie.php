61fa56362eef18a8a4bf1fdf02974ea30860225d
s24717 <s24717@pjwstk.edu.pl>
2022-06-17 20:26:12 +0200

    Initial commit

---
+++ logowanie.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+<meta charset="utf-8" />
+	<title>Sklep Internetowy</title>
+	<meta name="description" content="Serwis poświęcony sklepowi internetowemu i zaliczeniu projektu php" />
+	<meta name="keywords" content="sklep,internetowy,market,zakupy" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+</head>
+
+<body>
+
+    <form action="zaloguj.php" method="post">
+    Login: <br/><input type="text" name="login"/> <br/>
+    Haslo: <br/><input type="password" name="login"/> <br/>
+    <input type="submit" value="Zaloguj sie" />
+    </form>
+
+    
+
+</body>
+
+</html>
